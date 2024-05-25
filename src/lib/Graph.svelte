852cb1669e52337120e81aa383ffffcdf041be63
Arthur Delamare <arthur.delamare1@viacesi.fr>
2024-05-25 17:39:06 +0200

    feat: assign node with random position
    graphology-layout --> random

--- src/lib/Graph.svelte
+++ src/lib/Graph.svelte
@@ -1,18 +1,42 @@
 <script lang="ts">
   import Graph from "graphology";
+  import { random } from "graphology-layout";
   import Sigma from "sigma";
+  //   import type { NodeDisplayData } from "sigma/types";
   import { onMount } from "svelte";
 
   let container: HTMLElement;
 
   onMount(() => {
+    // Create a graph and assign nodes with random positions
     const graph = new Graph();
     graph.addNode("1", { label: "Node 1", x: 0, y: 0, size: 10, color: "blue" });
     graph.addNode("2", { label: "Node 2", x: 1, y: 1, size: 20, color: "red" });
-    graph.addEdge("1", "2", { size: 5, color: "purple" });
+    for (let i = 3; i < 10; i++) {
+      graph.addNode(`${i}`, { label: `Node ${i}`, size: 15, color: "red" });
+    }
+    random.assign(graph);
 
-    // Instantiate sigma.js and render the graph
-    const sigmaInstance = new Sigma(graph, container);
+    // Render the graph - binded on its container
+    const renderer = new Sigma(graph, container);
+
+    // Create node on click
+    renderer.addListener("clickStage", ({ event }) => {
+      const coordForGraph = renderer.viewportToGraph({ x: event.x, y: event.y });
+      graph.addNode(Math.random() * 100 + event.x + event.y, {
+        ...coordForGraph,
+        size: 10,
+        color: "red",
+      });
+    });
+
+    // sigmaInstance.setSetting("nodeReducer", (node, data) => {
+    //   const res: Partial<NodeDisplayData> = { ...data };
+    //   if (node === "1") {
+    //     res.color = "#BDBDBD";
+    //   }
+    //   return res;
+    // });
   });
 </script>
 
